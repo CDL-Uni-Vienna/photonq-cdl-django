0fa93c5e88d77d4f44d09b7497b303d6fc40a54a
Felix Zilk <zilk.felix@gmail.com>
2021-11-17 12:48:51 +0100

    create Wagtail project

---
+++ cdl_webservice/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-#8yff&k+w1)+!&$+u5bih2px#o9yh!9g=36_y5k8+_r9czp!gg'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass

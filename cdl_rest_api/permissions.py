5fa9e84512e9a0676a50690174477792020593a6
Felix Zilk <zilk.felix@gmail.com>
2021-11-24 15:08:06 +0100

    fix UpdateOwnProfile permission

--- cdl_rest_api/permissions.py
+++ cdl_rest_api/permissions.py
@@ -7,14 +7,16 @@ class UpdateOwnProfile(permissions.BasePermission):
     # Has_object_permission gets called every time a request is made
     # to the API that we assign our permission to
     # has_object_permission function defines permission class
-    def has_object_permission(self, request, obj):
+    def has_object_permission(self, request, view, obj):
         """Check if user is trying to edit their own profile"""
         # Allow users to view other users profiles but only able
         # to make changes to their own profile
         # Checks if the request is in the safe methods,
         # if it is, request is allowed to go through
-        if request.method in permissions.SAFE_METHODS:
-            return True
+
+        # if request.method in permissions.SAFE_METHODS:
+        #     return True
+
         # If it is not in the safe methods, the result of
         # the boolean expression is returned
         return obj.id == request.user.id
